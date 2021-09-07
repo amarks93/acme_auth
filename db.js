0d68a14ef638b7d5f75cdc623f8a678c73a40b89
Alexandra Marks <amarks93@gmail.com>
2021-09-07 13:00:14 -0400

    using JWT section

--- db.js
+++ db.js
@@ -1,72 +1,89 @@
 const Sequelize = require('sequelize');
 const { STRING } = Sequelize;
+const jwt = require('jsonwebtoken');
 const config = {
-  logging: false
+  logging: false,
 };
 
-if(process.env.LOGGING){
+// variable is JWT, and the value is tokenSecret
+// process.env is the object location
+const tokenSecret = process.env.JWT;
+
+if (process.env.LOGGING) {
   delete config.logging;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+  config
+);
 
 const User = conn.define('user', {
   username: STRING,
-  password: STRING
+  password: STRING,
 });
 
-User.byToken = async(token)=> {
+User.byToken = async (token) => {
   try {
-    const user = await User.findByPk(token);
-    if(user){
+    const userObject = await jwt.verify(token, tokenSecret);
+    console.log('userObject', userObject);
+    if (userObject) {
+      const user = await User.findByPk(userObject.userId);
+      console.log('user', user);
       return user;
     }
     const error = Error('bad credentials');
     error.status = 401;
     throw error;
-  }
-  catch(ex){
+  } catch (ex) {
     const error = Error('bad credentials');
     error.status = 401;
     throw error;
   }
 };
-User.authenticate = async({ username, password })=> {
-    const user = await User.findOne({
-      where: {
-        username,
-        password
-      }
-    });
-    if(user){
-      return user.id; 
-    }
-    const error = Error('bad credentials');
-    error.status = 401;
-    throw error;
-  };
-  
-  const syncAndSeed = async()=> {
-    await conn.sync({ force: true });
-    const credentials = [
-      { username: 'lucy', password: 'lucy_pw'},
-      { username: 'moe', password: 'moe_pw'},
-      { username: 'larry', password: 'larry_pw'}
-    ];
-    const [lucy, moe, larry] = await Promise.all(
-      credentials.map( credential => User.create(credential))
+User.authenticate = async ({ username, password }) => {
+  const user = await User.findOne({
+    where: {
+      username,
+      password,
+    },
+  });
+  if (user) {
+    const token = await jwt.sign(
+      {
+        userId: user.id,
+      },
+      tokenSecret
     );
-    return {
-      users: {
-        lucy,
-        moe,
-        larry
-      }
-    };
+    console.log('token', token);
+    return token;
+  }
+  const error = Error('bad credentials');
+  error.status = 401;
+  throw error;
+};
+
+const syncAndSeed = async () => {
+  await conn.sync({ force: true });
+  const credentials = [
+    { username: 'lucy', password: 'lucy_pw' },
+    { username: 'moe', password: 'moe_pw' },
+    { username: 'larry', password: 'larry_pw' },
+  ];
+  const [lucy, moe, larry] = await Promise.all(
+    credentials.map((credential) => User.create(credential))
+  );
+  return {
+    users: {
+      lucy,
+      moe,
+      larry,
+    },
   };
-  
-  module.exports = {
-    syncAndSeed,
-    models: {
-      User
-    }
-  };
+};
+
+module.exports = {
+  syncAndSeed,
+  models: {
+    User,
+  },
+};
